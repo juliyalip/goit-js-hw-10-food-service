dfc6a8a272ae7beb88f4307b03540ac2b7e747c1
Juliya Lipatova <575@ukr.net>
2020-11-03 18:52:48 +0200

    правки

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Привет, Мир!');
+import theme from './theme';
+import menuItem from './template/menu-item.hbs'; // vernet function 1 elemenl
+import menuItems from './template/menu-items.hbs'; // vernet function allMenu
+import cartFoods from './template/menu.json';
+
+
+const allMenuMarkup = createMenuItems(cartFoods); // razmetka html
+console.log(allMenuMarkup);
+
+const containerMenu = document.querySelector('.js-menu');
+console.log(typeof(allMenuMarkup))
+containerMenu.insertAdjacentHTML('beforeend', allMenuMarkup);
+
+
+function createMenuItems(cartFoods) {
+  return menuItems(cartFoods);
+}
